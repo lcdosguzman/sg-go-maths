88fa5338ad3097270aa41fd251c413e2b73ac41d
Simon <lcdosguzman@gmail.com>
2022-09-03 19:07:33 -0300

    add operations

--- sggomaths.go
+++ sggomaths.go
@@ -4,3 +4,18 @@ package sggomaths
 func Suma(num1, num2 int) int {
 	return num1 + num2
 }
+
+// Resta retorna la Resta de dos números enteros
+func Resta(num1, num2 int) int {
+	return num1 - num2
+}
+
+// Multiplica retorna la multiplicacion de dos números enteros
+func Multiplica(num1, num2 int) int {
+	return num1 * num2
+}
+
+// Divide retorna la división de dos números enteros
+func Divide(num1, num2 int) int {
+	return num1 / num2
+}
